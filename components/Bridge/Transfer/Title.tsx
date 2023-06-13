b08404c50b59325c3dccf8b595ae7c45655b8183
Ted Ian Osias <osiastedian@gmail.com>
2023-06-13 22:44:10 +0800

    update title for intialize state

--- components/Bridge/Transfer/Title.tsx
+++ components/Bridge/Transfer/Title.tsx
@@ -3,16 +3,10 @@ import { Typography } from "@mui/material";
 
 const TransferTitle = () => {
   const { transfer } = useTransfer();
-  if (transfer.status === "initialize") {
-    return (
-      <Typography variant="body1" sx={{ my: 3 }}>
-        New Transfer
-      </Typography>
-    );
-  }
+
   return (
     <Typography variant="body1" sx={{ my: 3 }}>
-      Transfer #{transfer.id}
+      Transfer #{transfer.status === "initialize" ? "--------" : transfer.id}
     </Typography>
   );
 };
