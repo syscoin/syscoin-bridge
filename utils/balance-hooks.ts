e9913efae9db031e0194f49be6eb8e994e497325
Ted Ian Osias <osiastedian@gmail.com>
2023-09-13 09:34:05 +0800

    fix: use web3 rpc first when fetching balance

--- utils/balance-hooks.ts
+++ utils/balance-hooks.ts
@@ -1,5 +1,6 @@
 import { BlockbookAPIURL } from "@contexts/Transfer/constants";
 import { isValidEthereumAddress } from "@pollum-io/sysweb3-utils";
+import { useWeb3 } from "components/Bridge/v3/context/Web";
 
 import { useQuery } from "react-query";
 
@@ -13,15 +14,26 @@ export const useUtxoBalance = (xpub?: string) =>
     return parseInt(balanceInText) / Math.pow(10, 8);
   });
 
-export const useNevmBalance = (address?: string) =>
-  useQuery(["nevm", "balance", address], async () => {
+export const useNevmBalance = (address?: string) => {
+  const web3 = useWeb3();
+  return useQuery(["nevm", "balance", address], async () => {
     if (!address) return Promise.resolve(0);
-    const url = `https://explorer.syscoin.org/api?module=account&action=eth_get_balance&address=${address}&tag=latest`;
-    const ethBalanceInHex = await fetch(url)
-      .then((res) => res.json())
-      .then((rpcResp) => rpcResp.result);
-    const valueAsNumber = parseInt(ethBalanceInHex, 16);
-    if (isNaN(valueAsNumber)) return Promise.resolve(0);
-    const ethBalance = valueAsNumber / Math.pow(10, 18);
+
+    let balRpc = await web3.eth.getBalance(address).then(parseInt).catch(() => undefined);
+
+    if (balRpc === undefined) {
+      const url = `https://explorer.syscoin.org/api?module=account&action=eth_get_balance&address=${address}&tag=latest`;
+      const ethBalanceInHex = await fetch(url)
+        .then((res) => res.json())
+        .then((rpcResp) => rpcResp.result);
+      const valueAsNumber = parseInt(ethBalanceInHex, 16);
+      if (isNaN(valueAsNumber)) {
+        return 0;
+      }
+      balRpc = valueAsNumber;
+    }
+
+    const ethBalance = balRpc / Math.pow(10, 18);
     return ethBalance;
   });
+};
