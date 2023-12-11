06b80a95226469640b70fb842e0709b7ad7dc0ff
Ted Ian Osias <osiastedian@gmail.com>
2023-12-11 23:54:39 +0800

    ci: remove missing MONGODB_URI warning

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -5,12 +5,6 @@ declare global {
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
 let cached = global.mongoose;
 
 if (!cached) {
