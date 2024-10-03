18559162e5849dbe2d478fe6b3d2c0212913c4c3
Ted Ian Osias <osiastedian@gmail.com>
2024-10-03 22:01:52 +0800

    deploy(rollux): transaction service logos

--- contexts/Transfer/constants.ts
+++ contexts/Transfer/constants.ts
@@ -1,7 +1,7 @@
 import { utils as syscoinUtils } from "syscoinjs-lib";
 export const SYSX_ASSET_GUID = "123456";
 const isTestnet = process.env.NEXT_PUBLIC_IS_TESTNET === "true";
-export const BlockbookAPIURL = process.env.NEXT_PUBLIC_BLOCKBOOK_API_URL;
+export const BlockbookAPIURL = process.env.NEXT_PUBLIC_BLOCKBOOK_API_URL!;
 
 export const UTXO_NETWORK = isTestnet
   ? syscoinUtils.syscoinNetworks.testnet
