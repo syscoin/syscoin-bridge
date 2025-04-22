3db8c7d7c1c0d0afaccf0f15d72ded1a1cd08826
Ted Ian Osias <osiastedian@gmail.com>
2025-04-23 06:30:05 +0800

    fix(pali-v2): add disclaimer to to remove default wallet

--- pages/api/flags.ts
+++ pages/api/flags.ts
@@ -4,9 +4,15 @@ function handler(req: NextApiRequest, res: NextApiResponse) {
   const foundationFundingAvailable = process.env.FOUNDATION_FUNDED === "true";
   const adminEnabled = process.env.ADMIN_API_KEY !== undefined;
   const isSys5Enabled = process.env.SYS5_ENABLED === "true";
+  const isPaliV2NevmEnabled = process.env.PALI_V2_NEVM_ENABLED === "true";
   return res
     .status(200)
-    .json({ foundationFundingAvailable, adminEnabled, isSys5Enabled });
+    .json({
+      foundationFundingAvailable,
+      adminEnabled,
+      isSys5Enabled,
+      isPaliV2NevmEnabled,
+    });
 }
 
 export default handler;
