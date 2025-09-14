1882686e97ae83982ab859c4ba8c4201ddad43d5
Ted Ian Osias <osiastedian@gmail.com>
2025-09-14 20:53:58 +0800

    fix(build): disable swcMinify to avoid duplicate label error in _app chunk

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: false,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
 
